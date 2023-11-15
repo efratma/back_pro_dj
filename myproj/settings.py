fe6cdd28a42b44daeb84ef2a02ed21a93f23e650
efratma <122476479+efratma@users.noreply.github.com>
2023-11-15 20:26:37 +0200

    Update settings.py

--- myproj/settings.py
+++ myproj/settings.py
@@ -108,7 +108,7 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     "corsheaders.middleware.CorsMiddleware",
-    'allauth.account.middleware.AccountMiddleware',
+
 
 
 ]
