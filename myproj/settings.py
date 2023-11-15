76b19a0785442b7773734ea8049c5b6213793bf1
efratma <122476479+efratma@users.noreply.github.com>
2023-11-15 20:00:50 +0200

    Update settings.py

--- myproj/settings.py
+++ myproj/settings.py
@@ -42,7 +42,6 @@ INSTALLED_APPS = [
     'rest_framework.authtoken',
     'rest_framework_simplejwt.token_blacklist',
     'rest_framework_simplejwt',
-    'allauth',
     'allauth.account',
     'allauth.socialaccount',
     'allauth.socialaccount.providers.google',
